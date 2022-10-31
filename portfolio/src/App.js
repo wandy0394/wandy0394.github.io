0ee2440d0ad19789fe089f61fae07c6a48c0c430
Dev <wandy0394@gmail.com>
2022-10-31 20:53:00 +1100

    basic page strcture

---
+++ portfolio/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='titlebar-contact'>
+        <div className='my-name'></div>
+        <div className='my-email'></div>
+      </div>
+
+      <div className='bio'>
+        <p className='bio-text'></p>
+      </div>
+
+      <div className='project-list'>
+        <div className='project'>
+
+        </div>
+        <div className='project'>
+          
+        </div>
+        <div className='project'>
+        
+        </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="utf-8"/>
+    <App />
+  </React.StrictMode>
+);
+
+
