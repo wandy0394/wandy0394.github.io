7a976ec2f69581d9665b10b79bf90f3199bf228b
Dev <wandy0394@gmail.com>
2022-11-01 21:40:25 +1100

    filled in project card with name, blurb and links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,22 +14,35 @@ function App() {
       </div>
 
       <div className='bio'>
-        <p className='bio-text'>This is some placeholder text that is as long as I am willing to make it plus a little bit more</p>
+        <p className='bio-text'>Hi, I'm <p className='bio-name'>Andy</p>.This is some placeholder text that is as long as I am willing to make it plus a little bit more</p>
+        <p className='bio-email'>bioEmail@email.com</p>
       </div>
       <hr></hr>
       <div className='project-list'>
         <div className='project'>
+          <div className='project-name'>Project Name 1</div>
+          <div className='project-blurb'>Blurb blurby blurb</div>
+          <div className='project-demo'>Try me</div>
+          <div className='project-source'>View on GitHub</div>
+        </div>
+        <div className='project'>
+        <div className='project-name'>Project Name 2</div>
+          <div className='project-blurb'>Blurb blurby blurb</div>
+          <div className='project-demo'>Try me</div>
+          <div className='project-source'>View on GitHub</div>          
+        </div>
+        <div className='project'>
+        <div className='project-name'>Project Name 3</div>
+          <div className='project-blurb'>Blurb blurby blurb</div>
+          <div className='project-demo'>Try me</div>
+          <div className='project-source'>View on GitHub</div>        
+        </div>
 
-        </div>
         <div className='project'>
-          
-        </div>
-        <div className='project'>
-        
-        </div>
-
-        <div className='project'>
-        
+        <div className='project-name'>Project Name 4</div>
+          <div className='project-blurb'>Blurb blurby blurb</div>
+          <div className='project-demo'>Try me</div>
+          <div className='project-source'>View on GitHub</div>        
         </div>
 
     </div>
