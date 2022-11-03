a581ce3a69861df7fa7725d93ea3d4ac6ee7a247
Dev <wandy0394@gmail.com>
2022-11-03 21:29:37 +1100

    added responsive grid to bio section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -15,11 +15,13 @@ function App() {
       </div>
 
       <div className='bio'>
-        <div className='bio-text'>Hi, I'm <p className='bio-name'>Andy</p>.
+        <div className='bio-greeting'>Hi, I'm <p className='bio-name'>Andy</p>.</div>
+        <div className='bio-email'><p>bioEmail@email.com</p></div>
+        <div className='bio-blurb'>
           <p className='bio-narrow'>These are my projects.</p>
           <p className='bio-wide'>This is some placeholder text that is as long as I am willing to make it plus a little bit more</p>
-        </div>
-        <p className='bio-email'>bioEmail@email.com</p>
+        </div>  
+        
       </div>
       <hr></hr>
       <div className='project-list'>
