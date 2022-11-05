8df44ccd3d6b6b8b46a8c7b56ca2746ecbd05433
Dev <wandy0394@gmail.com>
2022-11-05 15:48:29 +1100

    added tools page, separate style sheet into 3 sheets

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,40 @@
 
-import './AppVariant.css';
-import {AiOutlineHtml5} from 'react-icons/ai'
+// import './AppVariant.css';
+import './app-styles.css';
+import './project-styles.css';
+import './tool-styles.css';
+import {useEffect, useState} from 'react'
+import {AiOutlineConsoleSql, AiOutlineHtml5} from 'react-icons/ai'
 import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, } from 'react-icons/fa'
 
 function App() {
+
+  const pageProjectID = 'page-project';
+  const pageToolID = 'page-tool';
+  const [pageProjectSelected, setPageProjectSelected] = useState(true)
+  const [pageToolSelected, setPageToolSelected] = useState(false)
+
+  const clickProject = () => {
+    console.log('project')
+    setPageProjectSelected(true)
+    setPageToolSelected(false)
+  }
+
+  const clickTools = () => {
+    console.log('tools')
+    setPageProjectSelected(false)
+    setPageToolSelected(true)
+  }
+
+  useEffect(()=> {
+
+  }, [pageProjectSelected, pageToolSelected])
+
   return (
     <div className="App">
-      <div className='background-blur'></div>
       <nav>
-        <a href='#' className='nav-link left active'>Projects</a>
-        <a href='#' className='nav-link right'>Tools</a>
+        <a href='#' className='nav-link left active' onClick={clickProject}>Projects</a>
+        <a href='#' className='nav-link right' onClick={clickTools}>Tools</a>
       </nav>
       <div className='bio'>
         <div className='bio-header'>
@@ -22,54 +47,56 @@ function App() {
         </div>  
       </div>
       <hr></hr>
-      <div className='project-list'>
-        <div className='project'>
-          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
-            <div className='project-name'>Project Name 1</div>
-          <div className='project-blurb'>Blurb blurby blurb</div>
-          <div className='project-buttons'>
-            <div className='project-source button'>&lt;src /&gt;</div>
-            <div className='project-demo button'>Try me</div>
+      <div className={pageProjectSelected ? 'page-project active' :'page-project inactive'} id={pageProjectID}>
+        <div className='project-list'>
+          <div className='project'>
+            <div className='project-image'><IconHtml className='icon'/><IconCSS className='icon'/><IconReact className='icon'/></div>
+              <div className='project-name'>Project Name 1</div>
+            <div className='project-blurb'>Blurb blurby blurb</div>
+            <div className='project-buttons'>
+              <div className='project-source button'>&lt;src /&gt;</div>
+              <div className='project-demo button'>Try me</div>
+            </div>
+          </div>
+          <div className='project'>
+            <div className='project-image'><IconHtml className='icon'/><IconCSS className='icon'/><IconReact className='icon'/></div>
+              <div className='project-name'>Project Name 1</div>
+            <div className='project-blurb'>Blurb blurby blurb</div>
+            <div className='project-buttons'>
+              <div className='project-source button'>&lt;src /&gt;</div>
+              <div className='project-demo button'>Try me</div>
+            </div>
+          </div>
+          <div className='project'>
+            <div className='project-image'><IconHtml className='icon'/><IconCSS className='icon'/><IconReact className='icon'/></div>
+              <div className='project-name'>Project Name 1</div>
+            <div className='project-blurb'>Blurb blurby blurb</div>
+            <div className='project-buttons'>
+              <div className='project-source button'>&lt;src /&gt;</div>
+              <div className='project-demo button'>Try me</div>
+            </div>
+          </div>
+          <div className='project'>
+            <div className='project-image'><IconHtml className='icon'/><IconCSS className='icon'/><IconReact className='icon'/></div>
+              <div className='project-name'>Project Name 1</div>
+            <div className='project-blurb'>Blurb blurby blurb</div>
+            <div className='project-buttons'>
+              <div className='project-source button'>&lt;src /&gt;</div>
+              <div className='project-demo button'>Try me</div>
+            </div>
+          </div>
+          <div className='project'>
+            <div className='project-image'><IconHtml className='icon'/><IconCSS className='icon'/><IconReact className='icon'/></div>
+              <div className='project-name'>Project Name 1</div>
+            <div className='project-blurb'>Blurb blurby blurb</div>
+            <div className='project-buttons'>
+              <div className='project-source button'>&lt;src /&gt;</div>
+              <div className='project-demo button'>Try me</div>
+            </div>
           </div>
         </div>
-        <div className='project'>
-          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
-            <div className='project-name'>Project Name 1</div>
-          <div className='project-blurb'>Blurb blurby blurb</div>
-          <div className='project-buttons'>
-            <div className='project-source button'>&lt;src /&gt;</div>
-            <div className='project-demo button'>Try me</div>
-          </div>
-        </div>
-        <div className='project'>
-          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
-            <div className='project-name'>Project Name 1</div>
-          <div className='project-blurb'>Blurb blurby blurb</div>
-          <div className='project-buttons'>
-            <div className='project-source button'>&lt;src /&gt;</div>
-            <div className='project-demo button'>Try me</div>
-          </div>
-        </div>
-        <div className='project'>
-          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
-            <div className='project-name'>Project Name 1</div>
-          <div className='project-blurb'>Blurb blurby blurb</div>
-          <div className='project-buttons'>
-            <div className='project-source button'>&lt;src /&gt;</div>
-            <div className='project-demo button'>Try me</div>
-          </div>
-        </div>
-        <div className='project'>
-          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
-            <div className='project-name'>Project Name 1</div>
-          <div className='project-blurb'>Blurb blurby blurb</div>
-          <div className='project-buttons'>
-            <div className='project-source button'>&lt;src /&gt;</div>
-            <div className='project-demo button'>Try me</div>
-          </div>
-        </div>
-    </div>
-
+      </div>
+      <div className={pageToolSelected ? 'page-tool active' :'page-tool inactive'} id={pageToolID}>Tools go here</div>
     </div>
   );
 }
