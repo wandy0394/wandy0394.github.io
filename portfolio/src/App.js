73140f0189dfd99c3ddc0fb35069c994af4efa7b
Dev <wandy0394@gmail.com>
2022-11-05 12:00:20 +1100

    code cleanup, added icons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,35 +1,33 @@
 
 import './AppVariant.css';
+import {AiOutlineHtml5} from 'react-icons/ai'
+import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, } from 'react-icons/fa'
 
 function App() {
   return (
     <div className="App">
       <div className='background-blur'></div>
       <div className='titlebar-contact'>
-        <div className='my-name'>
-            <p className='label-name'>FirstName, LastName</p>
-        </div>
         <div className='my-email'>
           <p className='label-email'>email@email.com</p>
         </div>
       </div>
 
       <div className='bio'>
+        <div className='bio-header'>
         <div className='bio-greeting'>Hi, I'm <p className='bio-name'>Andy</p>.</div>
         <div className='bio-email'><p>bioEmail@email.com</p></div>
+        </div>
         <div className='bio-blurb'>
           <p className='bio-narrow'>These are my projects.</p>
           <p className='bio-wide'>This is some placeholder text that is as long as I am willing to make it plus a little bit more</p>
         </div>  
-        
       </div>
       <hr></hr>
       <div className='project-list'>
         <div className='project'>
-          <div className='project-image'></div>
-          <div className='project-header'>
+          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
             <div className='project-name'>Project Name 1</div>
-          </div>
           <div className='project-blurb'>Blurb blurby blurb</div>
           <div className='project-buttons'>
             <div className='project-source button'>&lt;src /&gt;</div>
@@ -37,10 +35,8 @@ function App() {
           </div>
         </div>
         <div className='project'>
-          <div className='project-image'></div>
-          <div className='project-header'>
+          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
             <div className='project-name'>Project Name 1</div>
-          </div>
           <div className='project-blurb'>Blurb blurby blurb</div>
           <div className='project-buttons'>
             <div className='project-source button'>&lt;src /&gt;</div>
@@ -48,10 +44,8 @@ function App() {
           </div>
         </div>
         <div className='project'>
-          <div className='project-image'></div>
-          <div className='project-header'>
+          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
             <div className='project-name'>Project Name 1</div>
-          </div>
           <div className='project-blurb'>Blurb blurby blurb</div>
           <div className='project-buttons'>
             <div className='project-source button'>&lt;src /&gt;</div>
@@ -59,10 +53,17 @@ function App() {
           </div>
         </div>
         <div className='project'>
-          <div className='project-image'></div>
-          <div className='project-header'>
+          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
             <div className='project-name'>Project Name 1</div>
+          <div className='project-blurb'>Blurb blurby blurb</div>
+          <div className='project-buttons'>
+            <div className='project-source button'>&lt;src /&gt;</div>
+            <div className='project-demo button'>Try me</div>
           </div>
+        </div>
+        <div className='project'>
+          <div className='project-image'><IconHtml class='icon'/><IconCSS class='icon'/><IconReact class='icon'/></div>
+            <div className='project-name'>Project Name 1</div>
           <div className='project-blurb'>Blurb blurby blurb</div>
           <div className='project-buttons'>
             <div className='project-source button'>&lt;src /&gt;</div>
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta charSet="utf-8"/>
     <App />
   </React.StrictMode>
 );
