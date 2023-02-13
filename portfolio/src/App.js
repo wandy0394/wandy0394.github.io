f530e714fee00c29fb509986a30b7c4cdd8ff2b5
Dev <wandy0394@gmail.com>
2023-02-13 21:27:30 +1100

    removed v1 styles, removed v1 content, installed tailwind

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,48 +1,23 @@
-import './app-styles.css';
-import './project-styles.css';
-import './tool-styles.css';
-import Tools from './Tools'
-import Bio from './Bio';
-import ProjectList from './ProjectList';
-import {useEffect, useState} from 'react'
-import {AiOutlineConsoleSql, AiOutlineHtml5} from 'react-icons/ai'
-import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, } from 'react-icons/fa'
-
+import './App.css'
 
 function App() {
 
-  const pageProjectID = 'page-project';
-  const pageToolID = 'page-tool';
-  const [pageProjectSelected, setPageProjectSelected] = useState(true)
-  const [pageToolSelected, setPageToolSelected] = useState(false)
 
-  const clickProject = () => {
-    console.log('project')
-    setPageProjectSelected(true)
-    setPageToolSelected(false)
-  }
-
-  const clickTools = () => {
-    console.log('tools')
-    setPageProjectSelected(false)
-    setPageToolSelected(true)
-  }
 
 
   return (
-    <div className="App">
-      <nav>
-        <a href='#' className={`nav-link left ${pageProjectSelected ? 'selected':''}`} onClick={clickProject}>Projects</a>
-        <a href='#' className={`nav-link right ${pageToolSelected ? 'selected':''}`}  onClick={clickTools}>Tools</a>
-      </nav>
-      <Bio/>
-      <hr></hr>
-      <ProjectList isactive={pageProjectSelected} />
-      <Tools isactive={pageToolSelected}/>
-      <hr></hr>
-      <div className='footer'></div>
+    <div className="h-screen">
+      <section id='header' className='h-screen bg-blue-200'>1</section>
+      <section id='description' className='h-screen bg-gray-200'>2</section>
+      <section id='experience' className='h-screen'>3</section>
+      <section id='project-list' className='h-screen'>4</section>
+      <section id='toolkit' className='h-screen'>5</section>
+      <section id='footer' className='h-screen'>6</section>
+
+
+      <div className='text-3xl'>Hello</div>
     </div>
-  );
+  )
 }
 
 export default App;
