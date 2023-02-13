5ddcac2ea9a7abb3effdc4a411630ec8cb0df6b1
Dev <wandy0394@gmail.com>
2023-02-14 07:16:42 +1100

    implemented header and navlinks

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,14 @@
 import './App.css'
 
+function NavLink({href, label}) {
+  return (
+    <a href={href} className='tab tab-bordered cursor-pointer hover:text-accent-focus hover:border-accent-focus'>
+      <p className='prose-xl'>{label}</p>
+    </a>
+
+  )
+}
+
 function App() {
 
 
@@ -7,15 +16,33 @@ function App() {
 
   return (
     <div className="h-screen">
-      <section id='header' className='h-screen bg-blue-200'>
-        <button className='btn btn-primary'>Button</button>
-      </section>
-      <section id='description' className='h-screen bg-gray-200'>2</section>
-      <section id='experience' className='h-screen'>3</section>
-      <section id='project-list' className='h-screen'>4</section>
-      <section id='toolkit' className='h-screen'>5</section>
-      <section id='footer' className='h-screen'>6</section>
+      <div className='relative'>
+        <section id='header' className='px-24 navbar fixed top-0 left-0'>
+          <div className='navbar-start prose w-1/2 md:w-1/4'>
+            <h1 className='text-accent'>Andy Wu</h1>
+          </div>
+          <div className='navbar-center'>
+          </div>
+          <div className='navbar-end w-1/2 md:w-3/4'>
+            <div className='hidden tabs gap-x-4 items-center justify-center md:flex'>
+              <NavLink href='#project-list' label='Projects'/>
+              <NavLink href='#experience' label='Experience'/>
+              <NavLink href='#skills' label='Skills'/>
+              <NavLink href='#tools' label='Tools'/>
+            </div>
+          </div>
+        </section>
+      </div>
+        <section id='description' className='h-screen pt-20'>2</section>
 
+        <section id='project-list' className='h-screen pt-20'>projects</section>
+        <section id='experience' className='h-screen pt-20'>exp</section>
+        <section id='skills' className='h-screen pt-20'>skills</section>
+      
+
+        <section id='tools' className='h-screen pt-20'>tools</section>
+
+        <section id='footer' className='h-screen'>6</section>
 
     </div>
   )
