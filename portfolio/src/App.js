8f4657e75ec3dfab11bbbce8b7399639b4b8b296
Dev <wandy0394@gmail.com>
2023-02-16 08:37:14 +1100

    basic scaffold for Tools section added. Minor adjustments to description hero

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import {Button, Drawer, Navbar} from 'react-daisyui'
 import { useState } from 'react'
+import {IoLogoJavascript as IconJavascript} from 'react-icons/io'
+import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, } from 'react-icons/fa'
 
 function NavLink({href, label}) {
   return (
@@ -12,12 +14,16 @@ function NavLink({href, label}) {
 
 function Section({id, children, classes}) {
   return (
-    <div id={id} className={`h-screen pt-20 w-full ${classes}`}>
+    <div id={id} className={`min-h-screen pt-20 w-full ${classes}`}>
       <div className='px-4 md:px-24 h-full w-full'>
         {children}
       </div>
     </div>
   )
+}
+
+function ToolCard({icon, label}) {
+
 }
 
 function App() {
@@ -53,12 +59,15 @@ function App() {
             </div> 
           </div>
           <Section id='description' classes='bg-base-200'>
-            <div className='flex items-center justify-center w-full h-full'>
-              <div className='flex flex-col gap-y-2 items-center justify-center'>
+            <div className='flex items-center justify-center w-full h-screen'>
+              <div className='flex flex-col gap-y-2 items-center justify-center w-full'>
                 <div className='prose'>
-                  <h1 >Hi, my name is <span className='text-accent'>Andy</span>.</h1>
+                  <h1 className='flex flex-col items-center justify-center'>Hey, it's<span className='text-accent'>Andy <span className='text-neutral-content'> here</span></span></h1>
                 </div>
-                <p className='prose-2xl'>I'm focused on learning new technologies to solve interesting challenges in different spaces.</p>
+                <div className='prose'>
+                  <h2>An Engineer</h2>
+                </div>
+                <p className='prose-2xl text-center'>Focusing on learning new skills and technologies. See a bit of my journey below.</p>
                 <div className='flex gap-x-4'>
                   <button className='btn btn-accent bg-teal-900 border-teal-900'>Resume</button>
                   <button className='btn text-white bg-slate-800'>Email me</button>
@@ -67,17 +76,82 @@ function App() {
             </div>
           </Section>
 
-          <Section id='project-list'>
-            MyProjects
-          </Section>
-          <Section id='experience' classes='bg-base-200'>
+          <Section id='experience'>
             Experience
+          </Section>
+          <Section id='project-list' classes='bg-base-200'>
+            MyProjects
           </Section>
           <Section id='skills'>
             Skills
           </Section>
           <Section id='tools' classes='bg-base-200'>
-            Tools
+            <div className='flex flex-col gap-y-8 items-center justify-center gap-2 h-full'>
+              <div className='prose'>
+                <h1>Tools</h1>
+              </div>
+              <div id='tool-set' className='flex gap-16 w-full items-start justify-center flex-wrap'>
+                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
+                    <div className='prose-xl'>Frontend</div>
+                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconHtml className='' color='#f06529'/>
+                        <div className='prose-2xl'>HTML5</div>
+                      </div>
+                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconCSS className='' color='#2965f1'/>
+                        <div className='prose-2xl'>CSS</div>
+                      </div>
+                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconJavascript className=''color='goldenrod'/>
+                        <div className='prose-2xl'>Javascript</div>
+                      </div>
+                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconReact className='' color='#61DBFB'/>
+                      <div className='prose-2xl'>ReactJS</div>
+                    </div>
+                  </div>
+
+                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
+                    <div className='prose-xl'>Backend</div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconNode className='' color='#68A063'/>
+                        <div className='prose-2xl'>NodeJS</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconCSS className='' color='#2965f1'/>
+                        <div className='prose-2xl'>mySQL</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconJavascript className=''color='goldenrod'/>
+                        <div className='prose-2xl'>Thing</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconReact className='' color='#61DBFB'/>
+                      <div className='prose-2xl'>Thing</div>
+                    </div>
+                  </div>
+
+                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
+                    <div className='prose-xl'>Languages</div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconNode className='' color='#68A063'/>
+                        <div className='prose-2xl'>NodeJS</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconCSS className='' color='#2965f1'/>
+                        <div className='prose-2xl'>mySQL</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconJavascript className=''color='goldenrod'/>
+                        <div className='prose-2xl'>Thing</div>
+                      </div>
+                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
+                        <IconReact className='' color='#61DBFB'/>
+                      <div className='prose-2xl'>Thing</div>
+                    </div>
+                  </div>                                  
+              </div>
+            </div>
           </Section>
         </div> 
         <div className="drawer-side h-full">
