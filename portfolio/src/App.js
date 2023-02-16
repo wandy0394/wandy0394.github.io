1a8047fc53e26a965e017fb18e55f8febe3e52c9
Dev <wandy0394@gmail.com>
2023-02-16 21:02:38 +1100

    Tools section filled in and styled

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,9 @@ import './App.css'
 import {Button, Drawer, Navbar} from 'react-daisyui'
 import { useState } from 'react'
 import {IoLogoJavascript as IconJavascript} from 'react-icons/io'
-import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, } from 'react-icons/fa'
+import {FaHtml5 as IconHtml, FaCss3Alt as IconCSS, FaReact as IconReact, FaNodeJs as IconNode, FaFileCode as IconScript, FaFileExcel as IconExcel} from 'react-icons/fa'
+import {SiPowershell as IconPowerShell, SiTypescript as IconTypescript} from 'react-icons/si'
+import {GrMysql as IconSQL} from 'react-icons/gr'
 
 function NavLink({href, label}) {
   return (
@@ -22,8 +24,20 @@ function Section({id, children, classes}) {
   )
 }
 
-function ToolCard({icon, label}) {
+function ToolCard({children}) {
+ return (
+  <div className='flex max-w-xs items-center gap-x-4 px-2 border-b border-solid  border-slate-500'>
+    {children}
+  </div>
+ )
+}
 
+function ToolSetCard({children}) {
+  return (
+    <div className='flex flex-col w-1/4 items-center rounded-2xl border gap-y-4 py-4 px-20 h-full'>
+      {children}
+    </div>
+  )
 }
 
 function App() {
@@ -85,71 +99,72 @@ function App() {
           <Section id='skills'>
             Skills
           </Section>
-          <Section id='tools' classes='bg-base-200'>
+          <Section id='tools' classes='bg-base-200 h-screen'>
             <div className='flex flex-col gap-y-8 items-center justify-center gap-2 h-full'>
               <div className='prose'>
                 <h1>Tools</h1>
               </div>
               <div id='tool-set' className='flex gap-16 w-full items-start justify-center flex-wrap'>
-                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
-                    <div className='prose-xl'>Frontend</div>
-                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconHtml className='' color='#f06529'/>
-                        <div className='prose-2xl'>HTML5</div>
-                      </div>
-                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconCSS className='' color='#2965f1'/>
-                        <div className='prose-2xl'>CSS</div>
-                      </div>
-                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconJavascript className=''color='goldenrod'/>
-                        <div className='prose-2xl'>Javascript</div>
-                      </div>
-                      <div className='flex max-w-xs items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconReact className='' color='#61DBFB'/>
-                      <div className='prose-2xl'>ReactJS</div>
-                    </div>
+                <ToolSetCard>
+                  <div className='prose-xl'>Frontend</div>
+                  <div className='flex max-w-xs w-full flex-col gap-y-4'>
+                    <ToolCard>
+                      <IconHtml size={30} className='' color='#f06529'/>
+                      <div className='prose-2xl'>HTML</div>
+                    </ToolCard>
+                    <ToolCard>
+                      <IconCSS size={30}  className='' color='#2965f1'/>
+                      <div className='prose-2xl'>CSS</div>
+                      </ToolCard>
+                    <ToolCard>
+                      <IconJavascript size={30}  className=''color='goldenrod'/>
+                      <div className='prose-2xl'>Javascript</div>
+                      </ToolCard>
+                    
+                    <ToolCard>
+                      <IconTypescript size={30}  className=''color='#007acc'/>
+                      <div className='prose-2xl'>Typescript</div>
+                      </ToolCard>
+                    <ToolCard>
+                      <IconReact size={30} className='' color='#61DBFB'/>
+                      <div className='prose-2xl'>React</div>
+                    </ToolCard>
                   </div>
-
-                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
-                    <div className='prose-xl'>Backend</div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconNode className='' color='#68A063'/>
-                        <div className='prose-2xl'>NodeJS</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconCSS className='' color='#2965f1'/>
-                        <div className='prose-2xl'>mySQL</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconJavascript className=''color='goldenrod'/>
-                        <div className='prose-2xl'>Thing</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconReact className='' color='#61DBFB'/>
-                      <div className='prose-2xl'>Thing</div>
-                    </div>
+                </ToolSetCard>
+                <ToolSetCard>
+                  <div className='prose-xl'>Backend</div>
+                  <div className='flex max-w-xs w-full flex-col gap-y-4'>
+                    <ToolCard>
+                      <IconNode size={30} className='' color='#68A063'/>
+                      <div className='prose-2xl'>NodeJS</div>
+                    </ToolCard>
+                    <ToolCard>
+                      <IconSQL size={30} className='' color='#2965f1'/>
+                      <div className='prose-2xl'>mySQL</div>
+                    </ToolCard>
                   </div>
-
-                  <div className='flex max-w-xs w-full flex-col gap-y-4 items-center rounded border py-4'>
-                    <div className='prose-xl'>Languages</div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconNode className='' color='#68A063'/>
-                        <div className='prose-2xl'>NodeJS</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconCSS className='' color='#2965f1'/>
-                        <div className='prose-2xl'>mySQL</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconJavascript className=''color='goldenrod'/>
-                        <div className='prose-2xl'>Thing</div>
-                      </div>
-                      <div className='flex items-center gap-x-4 px-2 border border-solid  border-slate-200'>
-                        <IconReact className='' color='#61DBFB'/>
-                      <div className='prose-2xl'>Thing</div>
-                    </div>
-                  </div>                                  
+                </ToolSetCard>
+                <ToolSetCard>
+                  <div className='prose-xl'>Scripting</div>
+                  <div className='flex max-w-xs w-full flex-col gap-y-4'>
+                    <ToolCard>
+                      <IconPowerShell size={30} className='' color='#008DED'/>
+                      <div className='prose-2xl'>PowerShell</div>
+                    </ToolCard>
+                    <ToolCard>
+                      <IconScript size={30} className='' color='#254768'/>
+                      <div className='prose-2xl'>AutoIt</div>
+                    </ToolCard>
+                    <ToolCard>
+                      <IconScript size={30} className=''color='grey'/>
+                      <div className='prose-2xl'>Bash</div>
+                    </ToolCard>
+                    <ToolCard>
+                      <IconExcel size={30} className='' color='#1d6f42'/>
+                      <div className='prose-2xl'>VBA</div>
+                    </ToolCard>
+                  </div>
+                </ToolSetCard>
               </div>
             </div>
           </Section>
